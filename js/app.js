c3f0083fab89bc209d6d7de971ed29dad1fa7c21
Daniel Prado <danieloprado@outlook.com>
2016-02-03 17:25:22 -0200

    teste

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-alert('Seja Bem vindo!');
+var app = angular.module("app", []);
+
+app.controller("appCtrl", function($scope) {
+  $scope.title = "Meu primeiro angular";
+  $scope.date = new Date();
+  $scope.cost = 5.6;
+});
